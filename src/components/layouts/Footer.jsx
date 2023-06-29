bf1faafc0ce1da1de70ef84b6785d29a840396b1
Lena <summyalena@gmail.com>
2023-06-29 13:47:52 +0100

    Create footer styles and links

---
+++ src/components/layouts/Footer.jsx
@@ -0,0 +1,48 @@
+
+import { FaEnvelope, FaPhone } from 'react-icons/fa';
+import footerLinks from '../utils/footer'
+import styles from './layout.module.css';
+
+function Footer() {
+  return (
+    <section className={`flex flex-col gap-md  ${styles.footer}`}>
+      <div className='flex flex-col gap-md'>
+      <div className='flex flex-col gap-sm'>
+      <h1>
+       Webxiel
+      </h1>
+      <p>Convenient and Professional service</p>
+      <ul className='flex gap-lg'>
+        {footerLinks.map((link) => (
+          <li key={link.id}>
+           <a href={link.link}>
+            <link.icon/>
+           </a>
+          </li>
+        ))}
+      </ul>
+      </div>
+      <div className='flex flex-col gap-sm'>
+        <h2>Contact info</h2>
+        <ul className='flex flex-col gap-sm'>
+          <li className='flex gap-sm'><FaPhone/> 09021526497</li>
+          <li className='flex gap-sm'><FaEnvelope/> webxiel@gmail.com</li>
+        </ul>
+      </div>
+      </div>
+      <hr/>
+      <div className={`flex ${styles.lastInfo}`}>
+          <span>
+            &copy;
+            {' '}
+            {new Date().getFullYear()}
+            {' '}
+            Webxiel
+            All rights reserved.
+          </span>
+        </div>
+    </section>
+  )
+}
+
+export default Footer
---
+++ src/components/utils/footer.js
@@ -0,0 +1,31 @@
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from 'react-icons/fa'
+
+
+const footerLinks = [
+  {
+    id: 1,
+    title: 'instagram',
+    icon: FaInstagram,
+    link: 'www.instagram.com'
+  },
+  {
+    id: 2,
+    title: 'Linkedin',
+    icon: FaLinkedin,
+    link: 'www.Linkedin.com'
+  },
+  {
+    id: 3,
+    title: 'Facebook',
+    icon: FaFacebook,
+    link: 'www.Facebook.com'
+  },
+  {
+    id: 4,
+    title: 'Twitter',
+    icon: FaTwitter,
+    link: 'www.twitter.com'
+  }
+]
+
+export default footerLinks;
