bfdeed1bf2b376836bc967aa2d603d3a129b87b6
Lena <summyalena@gmail.com>
2023-06-29 13:47:00 +0100

    Create home component

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Layout from '../layouts/Layout'
+import Herosection from './Herosection'
+
+function Home() {
+  return (
+    <Layout>
+      <Herosection/>
+    </Layout>
+  )
+}
+
+export default Home
