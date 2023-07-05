f7f0137bf7b323736068b266b30423ff102e47f1
Lena <summyalena@gmail.com>
2023-07-05 11:34:49 +0100

    Changes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,7 @@ import Herosection from './Herosection';
 import AboutSection from './AboutSection.jsx';
 import TrustedBy from './TrustedBy';
 import AppSection from './appSection/AppSection';
-import Services from './Services/Services';
+import Services from './services/Services';
 import Testimonials from './testimonials/Testimonials';
 import ContactSection from './ContactSection';
 
